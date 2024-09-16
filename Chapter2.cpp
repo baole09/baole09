4ac2c7ddb72785b0783d44508b91c9da51fff12f
baole09 <baonguyenle0910@gmail.com>
2024-09-16 23:16:41 +0700

    Chapter2.cpp

---
+++ Chapter2.cpp
@@ -0,0 +1,45 @@
+
+//2.1:
+#include <iostream>
+#include <math.h>
+using namespace std;
+/* Ktra n có phải số nguyên tố không?
+int main() {
+    int n, a = 0;
+    cout << "Nhap n= "; 
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            a++;
+        }
+    }
+
+    if (a == 2) {
+        cout << n << " la so nguyen to" << endl;
+    } else {
+        cout << n << " khong la so nguyen to" << endl;
+    }
+
+    return 0;
+}
+*/
+/*  Ktra n có phải số hoàn chỉnh khong?
+    int n,s=0;
+    cout<<"Nhap n= ";cin>>n;
+    for(int i=1; i<=n/2;i++;n%i==0){
+        s+=i;
+    }
+    if(s==n){
+        cout<<n<<" la so hoan chinh"<<endl;
+    }
+    else{
+        cout<<n<<" khong la so hoan chinh"<<endl;
+    }
+    return 0;
+}
+*/
+
+
+
+
